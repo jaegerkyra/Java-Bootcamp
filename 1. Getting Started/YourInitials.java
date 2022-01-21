e4563bdf0c03f4e1a71634883b6e8f32171b9ee2
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2022-01-21 11:58:01 -0600

    Uploading Java practice over lunch

---
+++ 1. Getting Started/YourInitials.java
@@ -0,0 +1,14 @@
+public class YourInitials {
+    public static void main(String[] args) {
+
+        //Enter your code here to print your initials
+        //My initials: KJ
+        System.out.println("K    K   JJJJJ");
+        System.out.println("K   K      J  ");
+        System.out.println("K  K       J  ");
+        System.out.println("KK         J  ");
+        System.out.println("K  K       J  ");
+        System.out.println("K   K   J  J  ");
+        System.out.println("K    K   JJJ  ");
+    }
+}

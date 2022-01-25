ca260f8432d92c257cbcab360070f36df34e171f
Kyra Jaeger <87786202+jaegerkyra@users.noreply.github.com>
2022-01-25 07:27:54 -0600

    Uploading morning work

---
+++ Workbooks and Challenges/Module 1/2. Variables/JoiningStrings.java
@@ -0,0 +1,11 @@
+public class JoiningStrings { 
+    public static void main(String[] args) {
+        String nameSentence = "His name is: ";
+        String placeholder = "Harry";
+        System.out.println(nameSentence+placeholder);
+
+        int points = 50;
+        String announcement = " points for Gryffindor";
+        System.out.println(points+announcement);
+    }
+}
